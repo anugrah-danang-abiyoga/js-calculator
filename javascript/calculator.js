8d7fcf80bc87f537a10437438321b4018b956cef
anugrah-danang <danangabiyoga26@gmail.com>
2018-03-03 21:30:39 +1300

    initial commit

--- javascript/calculator.js
+++ javascript/calculator.js
@@ -6,8 +6,10 @@ function pushBtn(obj) {
     var pushBtn = obj.innerHTML;
 
     if (pushed == '=') {
+        // Calculate
         inputLabel.innerHTML = eval(inputLabel.innerHTML);
     } else if (pushed == 'AC') {
+        // All Clear
         inputLabel.innerHTML = '0';
     } else {
         if (inputLabel.innerHTML = '0') {
